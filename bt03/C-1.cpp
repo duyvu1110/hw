5b26b83be6d9dbd699fd90c55a4941678e850628
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2022-03-05 17:03:00 +0700

    .

---
+++ bt03/C-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    set<int> s;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        s.insert(a[i]);
+    }
+    if(s.size()<n){
+        cout<<"yes";
+    }else{
+        cout<<"no";
+    }
+    return 0;
+}
